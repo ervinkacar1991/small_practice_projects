7b8e761fdd9156559d927f7872080a9022d29144
ervinkacar1991 <kacarervin@gmail.com>
2022-08-13 13:50:27 +0200

    added input handlers

--- basic-registration-form/src/App.js
+++ basic-registration-form/src/App.js
@@ -1,11 +1,60 @@
-import "./App.css";
+import React, { useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const handleFirstName = (e) => {
+    setValues({ ...values, firstName: e.target.value });
+  };
+  const handleLastName = (e) => {
+    setValues({ ...values, lastName: e.target.value });
+  };
+  const handleEmail = (e) => {
+    setValues({ ...values, email: e.target.value });
+  };
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div class="form-container">
+      <form class="register-form">
+        <input
+          onChange={handleFirstName}
+          value={values.firstName}
+          id="first-name"
+          className="form-field"
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+        />
+
+        <input
+          onChange={handleLastName}
+          value={values.lastName}
+          id="last-name"
+          className="form-field"
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+        />
+
+        <input
+          onChange={handleEmail}
+          value={values.emai}
+          id="email"
+          className="form-field"
+          type="text"
+          placeholder="Email"
+          name="email"
+        />
+
+        <button class="form-field" type="submit">
+          Register
+        </button>
+      </form>
     </div>
   );
 }
-
-export default App;
