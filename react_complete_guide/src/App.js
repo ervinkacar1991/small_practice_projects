76b46d6c66da66bbe10c8d1008c0779da9793025
ervinkacar1991 <kacarervin@gmail.com>
2022-08-16 13:34:09 +0200

    lifting the state up

--- react_complete_guide/src/App.js
+++ react_complete_guide/src/App.js
@@ -23,9 +23,13 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+  const addExpenseHandler = () => {
+    console.log("in app");
+    console.log(expenses);
+  };
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpenseData={addExpenseHandler} />
       <Expenses expenses={expenses} />
     </div>
   );
