ffb8680762f68b7de4d8981b550cbe55e50ce2f4
ervinkacar1991 <kacarervin@gmail.com>
2022-08-18 14:45:24 +0200

    outputting conditional content

--- react_complete_guide/src/components/Expenses/Expenses.js
+++ react_complete_guide/src/components/Expenses/Expenses.js
@@ -11,6 +11,22 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -18,14 +34,8 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {filteredExpenses.length === 0 && expensesContent}
+        {filteredExpenses.length > 0 && expensesContent}
       </Card>
     </div>
   );
