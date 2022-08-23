2d9bbf5066600fc277411036e80c1609385fadf3
ervinkacar1991 <kacarervin@gmail.com>
2022-08-23 19:37:34 +0200

    adding context and reducer

--- shopping_cart/src/components/Home.js
+++ shopping_cart/src/components/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { CartState } from "../context/Context";
 
 const Home = () => {
-  return <div>Home</div>;
+  const {
+    state: { products },
+  } = CartState();
+  console.log(products);
+  return (
+    <div className="home">
+      {/* <Filters /> */}
+      <div className="productContainer">
+        {products.map((prod) => (
+          <span>{prod.name}</span>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
