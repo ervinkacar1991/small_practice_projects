4b7992ad486829b6cbd06d9034ee7b555cd9e083
ervinkacar1991 <kacarervin@gmail.com>
2022-08-26 10:52:03 +0200

    adding logic for remove and add button

--- shopping_cart/src/components/SingleProduct.js
+++ shopping_cart/src/components/SingleProduct.js
@@ -23,11 +23,25 @@ const SingleProduct = ({ prod }) => {
             )}
             <Rating rating={prod.ratings} />
           </Card.Subtitle>
-          {cart.some((p) => p.id === prod.id)}
-          <Button variant="danger">Remove from cart</Button>
-          <Button disabled={!prod.inStock}>
-            {!prod.inStock ? "Out of Stock" : "Add to Cart"}
-          </Button>
+          {cart.some((p) => p.id === prod.id) ? (
+            <Button
+              onClick={() => {
+                dispatch({ type: "REMOVE_FROM_CART", payload: prod });
+              }}
+              variant="danger"
+            >
+              Remove from cart
+            </Button>
+          ) : (
+            <Button
+              onClick={() => {
+                dispatch({ type: "ADD_TO_CART", payload: prod });
+              }}
+              disabled={!prod.inStock}
+            >
+              {!prod.inStock ? "Out of Stock" : "Add to Cart"}
+            </Button>
+          )}
         </Card.Body>
       </Card>
     </div>
