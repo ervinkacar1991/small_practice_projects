25cd47b5a6408561c343c45410db9b7ff17d1089
Ervin Kacar <kacarervin@gmail.com>
2022-09-13 16:18:07 +0200

    Finish dropdown menu

---
+++ dropdown-select-menu/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Selector from "./components/Selector";
+
+function App() {
+  return (
+    <div className="bg-gray-900 font-Poppins flex  h-screen items-center justify-center">
+      <Selector />
+    </div>
+  );
+}
+
+export default App;
